0c088932d754247916bb0967a3fbae174d49876c
Travis CI <travis@travis-ci.org>
2017-01-21 21:03:03 +0200

    2

--- git_push.sh
+++ git_push.sh
@@ -3,7 +3,7 @@
 setup_git() {
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis CI"
-  git clone git@github.com:ScalecubePerf/ScalecubePerf.github.io.git
+  git clone https://github.com/ScalecubePerf/ScalecubePerf.github.io.git
 }
 
 commit_website_files() {
