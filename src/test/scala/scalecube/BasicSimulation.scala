8eb9ba316e34e5579bfbdd56961bed4f23ffff16
Travis CI <travis@travis-ci.org>
2017-01-21 22:27:22 +0200

    run test

--- src/test/scala/scalecube/BasicSimulation.scala
+++ src/test/scala/scalecube/BasicSimulation.scala
@@ -2,28 +2,17 @@ package scalecube
 
 import io.gatling.core.Predef._
 import io.gatling.http.Predef._
+
 import scala.concurrent.duration._
 
 class BasicSimulation extends Simulation {
 
-  val httpConf = http
-    .baseURL("http://computer-database.gatling.io") // Here is the root for all relative URLs
-    .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8") // Here are the common headers
-    .acceptEncodingHeader("gzip, deflate")
-    .acceptLanguageHeader("en-US,en;q=0.5")
-    .userAgentHeader("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:16.0) Gecko/20100101 Firefox/16.0")
+    io.scalecube.scalecube.performance.App.main(null)
+   	
+	val scn = scenario("scalecube-greeting").repeat(50, "count") { 
+		exec(http("greeting").get("http://localhost:8080/do"))
+	}
 
-  val scn = scenario("Scenario Name") // A scenario is a chain of requests and pauses
-    .exec(http("localhost").get("http://localhost:8080/do"))
-    
-  
-   setUp( 
-    scn.inject(
-	    nothingFor(1 seconds), // 1
-	    atOnceUsers(1), // 2
-	    rampUsers(1) over(1 seconds), // 3
-	    constantUsersPerSec(1) during(1 seconds), // 4
-	    rampUsers(1) over(20 seconds), // 3
-	    constantUsersPerSec(1) during(20 seconds) randomized // 5
-    )).protocols(httpConf)
+	setUp(scn.inject(rampUsers(5000) over (180 seconds)))
+
 }
